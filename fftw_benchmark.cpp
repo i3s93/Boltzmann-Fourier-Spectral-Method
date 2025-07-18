b2d8d617531f9d82f889c75cd585cd5fbca5a63f
William Sands <wsands@udel.edu>
2025-07-17 21:06:36 -0700

    CMake build system now includes the threaded version of FFTW, mostly for benchmarking purposes. The fftw benchmarks have been revised to also compare planning methods. Also, we updated the slurm script, specifically changing the bindings to help improve the bandwidth for the memory bound components. Since we are repeating this experiment, we can delete the old data.

--- fftw_benchmark.cpp
+++ fftw_benchmark.cpp
@@ -68,7 +68,7 @@ int main(int argc, char** argv) {
     const int grid_size = Nv * Nv * Nv;
     const int batch_size = Ns * Nv;
     double scale_factor = 1.0 / grid_size;
-    std::complex<double>* f_bkw = (std::complex<double>*)fftw_malloc(grid_size * sizeof(std::complex<double>));
+    double* f_bkw = fftw_alloc_real(grid_size);
     
     #pragma omp parallel for simd collapse(3)
     for (int i = 0; i < Nv; ++i){
@@ -83,9 +83,9 @@ int main(int argc, char** argv) {
     }
 
     // Allocate storage for the batching experiment
-    std::complex<double>* f     = (std::complex<double>*)fftw_malloc(batch_size * grid_size * sizeof(std::complex<double>));
-    std::complex<double>* f_hat = (std::complex<double>*)fftw_malloc(batch_size * grid_size * sizeof(std::complex<double>));
-    
+    fftw_complex* f = fftw_alloc_complex(batch_size * grid_size);
+    fftw_complex* f_hat = fftw_alloc_complex(batch_size * grid_size);
+
     // Fill the batch with the same distribution function
     #pragma omp parallel for simd collapse(4)
     for (int b = 0; b < batch_size; ++b) {
@@ -94,13 +94,13 @@ int main(int argc, char** argv) {
                 for (int k = 0; k < Nv; ++k){
                     int idx3 = (i * Nv + j) * Nv + k;
                     int idx4 = b * grid_size + (i * Nv + j) * Nv + k;
-                    f[idx4] = f_bkw[idx3];
+                    f[idx4][0] = f_bkw[idx3];
+                    f[idx4][1] = f_bkw[idx3];
                 }
             }
         }
     }
 
-/*
     // Approach 1: Use FFTW with the plan many interface for batched 3D transforms
     fftw_init_threads();
     fftw_plan_with_nthreads(omp_get_max_threads());    
@@ -115,17 +115,13 @@ int main(int argc, char** argv) {
     int *onembed = batched_dims; // The array is not embedded in a larger array
 
     fftw_plan fft_batch_plan_many = fftw_plan_many_dft(batched_rank, batched_dims, batch_size,
-                                    reinterpret_cast<fftw_complex*>(f), 
-                                    inembed, istride, idist,
-                                    reinterpret_cast<fftw_complex*>(f_hat),
-                                    onembed, ostride, odist,
+                                    f, inembed, istride, idist,
+                                    f_hat, onembed, ostride, odist,
                                     FFTW_FORWARD, FFTW_ESTIMATE);
 
     fftw_plan ifft_batch_plan_many = fftw_plan_many_dft(batched_rank, batched_dims, batch_size,
-                                    reinterpret_cast<fftw_complex*>(f_hat), 
-                                    inembed, istride, idist,
-                                    reinterpret_cast<fftw_complex*>(f),
-                                    onembed, ostride, odist,
+                                    f_hat, inembed, istride, idist,
+                                    f, onembed, ostride, odist,
                                     FFTW_BACKWARD, FFTW_ESTIMATE);
 
     std::vector<double> times_plan_many;
@@ -144,7 +140,8 @@ int main(int argc, char** argv) {
             for (int j = 0; j < Nv; ++j){
                 for (int k = 0; k < Nv; ++k){
                     int idx4 = b * grid_size + (i * Nv + j) * Nv + k;
-                    f_hat[idx4] *= scale_factor;
+                    f_hat[idx4][0] *= scale_factor;
+                    f_hat[idx4][1] *= scale_factor;
                 }
             }
         }
@@ -162,7 +159,7 @@ int main(int argc, char** argv) {
                 for (int k = 0; k < Nv; ++k){
                     int idx3 = (i * Nv + j) * Nv + k;
                     int idx4 = b * grid_size + (i * Nv + j) * Nv + k;
-                    L1_error_plan_many += std::abs(f_bkw[idx3] - f[idx4].real());
+                    L1_error_plan_many += std::abs(f_bkw[idx3] - f[idx4][0]);
                 }
             }
         }
@@ -173,26 +170,16 @@ int main(int argc, char** argv) {
     std::cout << "L1 error: " << L1_error_plan_many << "\n";
     print_stats_summary("Plan many", times_plan_many);
 
-*/
-
-    // Approach 2: Use FFTW with the new array execute interface
+    // Approach 2: Use FFTW with the new array execute interface (with FFTW_ESTIMATE)
     // We use dummy 3D arrays to create plans for the forward and inverse transforms 
-    std::complex<double>* data     = (std::complex<double>*)fftw_malloc(grid_size * sizeof(std::complex<double>));
-    std::complex<double>* data_hat = (std::complex<double>*)fftw_malloc(grid_size * sizeof(std::complex<double>));
+    fftw_complex* data = fftw_alloc_complex(grid_size);
+    fftw_complex* data_hat = fftw_alloc_complex(grid_size);
 
     // Each FFT/iFFT will be single threaded, but will parallelize over batches
     fftw_plan_with_nthreads(1);
 
-    fftw_plan fft_3d_plan  = fftw_plan_dft_3d(Nv, Nv, Nv,
-                                          reinterpret_cast<fftw_complex*>(data),
-                                          reinterpret_cast<fftw_complex*>(data_hat),
-                                          FFTW_FORWARD, FFTW_ESTIMATE);
-
-    fftw_plan ifft_3d_plan = fftw_plan_dft_3d(Nv, Nv, Nv,
-                                           reinterpret_cast<fftw_complex*>(data_hat),
-                                           reinterpret_cast<fftw_complex*>(data),
-                                           FFTW_BACKWARD, FFTW_ESTIMATE);
-
+    fftw_plan fft_3d_plan  = fftw_plan_dft_3d(Nv, Nv, Nv, data, data_hat, FFTW_FORWARD, FFTW_ESTIMATE);
+    fftw_plan ifft_3d_plan = fftw_plan_dft_3d(Nv, Nv, Nv, data_hat, data, FFTW_BACKWARD, FFTW_ESTIMATE);
 
      std::vector<double> times_manual_batch;
      times_manual_batch.reserve(trials);
@@ -209,9 +196,7 @@ int main(int argc, char** argv) {
 
             #pragma omp for
             for(int b = 0; b < batch_size; ++b){
-                fftw_execute_dft(fft_3d_plan, 
-                                reinterpret_cast<fftw_complex*>(f + b * grid_size), 
-                                reinterpret_cast<fftw_complex*>(f_hat + b * grid_size));
+                fftw_execute_dft(fft_3d_plan, f + b * grid_size, f_hat + b * grid_size); 
             }
 
             #pragma omp master
@@ -227,8 +212,9 @@ int main(int argc, char** argv) {
          for (int i = 0; i < Nv; ++i){
              for (int j = 0; j < Nv; ++j){
                  for (int k = 0; k < Nv; ++k){
-                     int idx4 = b * grid_size + (i * Nv + j) * Nv + k;
-                     f_hat[idx4] *= scale_factor;
+                    int idx4 = b * grid_size + (i * Nv + j) * Nv + k;
+                    f_hat[idx4][0] *= scale_factor;
+                    f_hat[idx4][1] *= scale_factor;
                  }
              }
          }
@@ -237,9 +223,7 @@ int main(int argc, char** argv) {
      // Take the inverse transform and measure the error
     #pragma omp parallel for
     for(int b = 0; b < batch_size; ++b){
-        fftw_execute_dft(ifft_3d_plan, 
-                        reinterpret_cast<fftw_complex*>(f_hat + b * grid_size), 
-                        reinterpret_cast<fftw_complex*>(f + b * grid_size));
+        fftw_execute_dft(ifft_3d_plan, f_hat + b * grid_size, f + b * grid_size); 
     }
 
     double L1_error_manual_batch = 0.0;
@@ -251,30 +235,110 @@ int main(int argc, char** argv) {
                  for (int k = 0; k < Nv; ++k){
                      int idx3 = (i * Nv + j) * Nv + k;
                      int idx4 = b * grid_size + (i * Nv + j) * Nv + k;
-                     L1_error_manual_batch += std::abs(f_bkw[idx3] - f[idx4].real());
+                     L1_error_manual_batch += std::abs(f_bkw[idx3] - f[idx4][0]);
                  }
              }
          }
      }
  
      L1_error_manual_batch *= dv * dv * dv;
-     std::cout << "Approximation error (manual batching):\n";
+     std::cout << "Approximation error (manual batching with FFTW_ESTIMATE):\n";
      std::cout << "L1 error: " << L1_error_manual_batch << "\n";
-     print_stats_summary("Manual batching", times_manual_batch);
+     print_stats_summary("Manual batching with FFTW_ESTIMATE", times_manual_batch);
+
+    // Approach 3: Use FFTW with the new array execute interface (with FFTW_EXHAUSTIVE)
+    // We use dummy 3D arrays to create plans for the forward and inverse transforms 
+
+    // Check if there is a wisdom file available
+    std::string wisdom_fname = "/global/homes/w/wsands/Projects/Boltzmann-Fourier-Spectral-Method/Build/fftw_wisdom.dat";
+    if (fftw_import_wisdom_from_filename(wisdom_fname.c_str()) == 0) {
+        std::cout << "Failed to import wisdom from file: " << wisdom_fname << "\n";
+    }
+
+
+    // Each FFT/iFFT will be single threaded, but will parallelize over batches
+    fftw_plan_with_nthreads(1);
+
+    fft_3d_plan  = fftw_plan_dft_3d(Nv, Nv, Nv, data, data_hat, FFTW_FORWARD, FFTW_EXHAUSTIVE);
+    ifft_3d_plan = fftw_plan_dft_3d(Nv, Nv, Nv, data_hat, data, FFTW_BACKWARD, FFTW_EXHAUSTIVE);
+
+    // Export wisdom immediately after plan creation using the specified filename
+    fftw_export_wisdom_to_filename(wisdom_fname.c_str());
+
+     std::vector<double> times_manual_batch_optimal;
+     times_manual_batch_optimal.reserve(trials);
+
+     // Note: We should ignore the time to startup threads
+     for(int trial_idx = 0; trial_idx < trials; ++trial_idx){
+
+        #pragma omp parallel
+        {
+            double start_time, end_time;
+
+            #pragma omp master
+            start_time = omp_get_wtime();
+
+            #pragma omp for
+            for(int b = 0; b < batch_size; ++b){
+                fftw_execute_dft(fft_3d_plan, f + b * grid_size, f_hat + b * grid_size); 
+            }
+
+            #pragma omp master
+            {
+                end_time = omp_get_wtime();
+                times_manual_batch_optimal.push_back(end_time - start_time);
+            }
+        }
+     }
+ 
+     #pragma omp parallel for simd collapse (4)
+     for (int b = 0; b < batch_size; ++b) {
+         for (int i = 0; i < Nv; ++i){
+             for (int j = 0; j < Nv; ++j){
+                 for (int k = 0; k < Nv; ++k){
+                    int idx4 = b * grid_size + (i * Nv + j) * Nv + k;
+                    f_hat[idx4][0] *= scale_factor;
+                    f_hat[idx4][1] *= scale_factor;
+                 }
+             }
+         }
+     }
+ 
+     // Take the inverse transform and measure the error
+    #pragma omp parallel for
+    for(int b = 0; b < batch_size; ++b){
+        fftw_execute_dft(ifft_3d_plan, f_hat + b * grid_size, f + b * grid_size); 
+    }
+
+    double L1_error_manual_batch_optimal = 0.0;
+ 
+     #pragma omp parallel for simd collapse (4) reduction(+:L1_error_manual_batch_optimal)
+     for (int b = 0; b < batch_size; ++b) {
+         for (int i = 0; i < Nv; ++i){
+             for (int j = 0; j < Nv; ++j){
+                 for (int k = 0; k < Nv; ++k){
+                     int idx3 = (i * Nv + j) * Nv + k;
+                     int idx4 = b * grid_size + (i * Nv + j) * Nv + k;
+                     L1_error_manual_batch_optimal += std::abs(f_bkw[idx3] - f[idx4][0]);
+                 }
+             }
+         }
+     }
+ 
+     L1_error_manual_batch_optimal *= dv * dv * dv;
+     std::cout << "Approximation error (manual batching with FFTW_EXHAUSTIVE):\n";
+     std::cout << "L1 error: " << L1_error_manual_batch_optimal << "\n";
+     print_stats_summary("Manual batching with FFTW_EXHAUSTIVE", times_manual_batch_optimal);
 
     // Free any allocated memory for arrays and plans
     fftw_free(f_bkw);
-    
     fftw_free(f);
     fftw_free(f_hat);
-    
     fftw_free(data);
     fftw_free(data_hat);
 
-/*
     fftw_destroy_plan(fft_batch_plan_many);
     fftw_destroy_plan(ifft_batch_plan_many);
-*/  
     fftw_destroy_plan(fft_3d_plan);
     fftw_destroy_plan(ifft_3d_plan);
   
